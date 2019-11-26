074f4a0ea68a33bf7e9be8d9b2700d5ca1f945f8
KAWSER04 <37189342+KAWSER04@users.noreply.github.com>
2019-11-26 12:50:24 +0600

    Add files via upload
    upper to lower string

---
+++ UPPER_TO_LOWER.asm
@@ -0,0 +1,43 @@
+.MODEL SMALL
+.STACK 100H
+.CODE
+MAIN PROC  
+   
+MOV AH, 1
+INT 21H
+ 
+MOV BL, AL
+ 
+MOV AH, 2
+MOV DL, 0AH
+INT 21H
+MOV DL, 0DH
+INT 21H
+ 
+MOV AL, BL
+       
+CMP AL, 'a'
+JGE LOWER
+CMP Al,'A'
+JL DISP
+CMP AL ,'Z'
+JG DISP
+ADD AL, 20H
+JMP DISP
+ 
+LOWER:
+    CMP AL ,'z'
+    JLE LOW
+    JMP DISP
+    LOW:
+        SUB AL, 20H
+        JMP DISP
+ 
+DISP:        
+    MOV DL, AL
+    MOV AH,2
+    INT 21H
+    MOV AH, 4CH
+    INT 21H
+    MAIN ENDP
+    END MAIN
